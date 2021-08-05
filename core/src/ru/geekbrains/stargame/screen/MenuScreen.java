e58bd120782165269fa2a7a42c1b5a35deaa9bc6
Elena Kozlovskaya <lenadk23@gmail.com>
2021-08-05 16:56:29 +0500

    lesson_3 classwork

--- core/src/ru/geekbrains/stargame/screen/MenuScreen.java
+++ core/src/ru/geekbrains/stargame/screen/MenuScreen.java
@@ -1,65 +1,48 @@
 package ru.geekbrains.stargame.screen;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Vector2;
 
-import ru.geekbrains.stargame.base.BaseScreen;
+import ru.gb.base.BaseScreen;
+import ru.geekbrains.stargame.math.Rect;
+import ru.geekbrains.stargame.sprite.Background;
 
 public class MenuScreen extends BaseScreen {
 
-    private Texture img;
-    private Vector2 position;
-    private Vector2 clickPosition;
-    private static final float SPEED = 0.5f;
-    private Vector2 velocity;
-    private Vector2 clickPosCopy;
+    private Texture bg;
+    private Background background;
 
+    private Vector2 pos;
 
     @Override
     public void show() {
         super.show();
-        img = new Texture("badlogic.jpg");
-        position = new Vector2(); // позиция картинки
-        clickPosition = new Vector2(); //позиция клика
-        velocity = new Vector2(); // скорость (расстояние между позициями двух векторов)
-        clickPosCopy = new Vector2();
+        bg = new Texture("textures/bg.png");
+        background = new Background(bg);
+        pos = new Vector2();
+    }
 
-
+    @Override
+    public void resize(Rect worldBounds) {
+        background.resize(worldBounds);
     }
 
     @Override
     public void render(float delta) {
         super.render(delta);
         batch.begin();
-        batch.draw(img, position.x, position.y);
+        background.draw(batch);
         batch.end();
-        clickPosCopy.set(clickPosition);
-        if(clickPosCopy.sub(position).len() > SPEED){
-            position.add(velocity);
-        } else {
-            position.set(clickPosition);
-        }
-
     }
 
     @Override
     public void dispose() {
         super.dispose();
-        img.dispose();
+        bg.dispose();
     }
 
     @Override
-    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        clickPosition.set(screenX, Gdx.graphics.getHeight() - screenY); // переворот координат
-        velocity.set(clickPosition.cpy().sub(position)).scl(SPEED);
-        return super.touchDown(screenX, screenY, pointer, button);
-    }
-
-    @Override
-    public boolean touchDragged(int screenX, int screenY, int pointer) {
-        clickPosition.set(screenX, Gdx.graphics.getHeight() - screenY); // переворот координат
-        velocity.set(clickPosition.cpy().sub(position)).scl(SPEED);
-        return super.touchDragged(screenX, screenY, pointer);
+    public boolean touchDown(Vector2 touch, int pointer, int button) {
+        return super.touchDown(touch, pointer, button);
     }
 }
